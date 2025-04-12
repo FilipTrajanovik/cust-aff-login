990cb48036a983afd29a16da865c355c4f6e05a2
Filip <filiptrajanovik@gmail.com>
2025-04-12 11:05:27 +0200

    settings.py updated

--- customerAffLogin/settings.py
+++ customerAffLogin/settings.py
@@ -122,7 +122,8 @@ STATICFILES_DIRS=[
     os.path.join(BASE_DIR, 'custLogin/templates/static'),
 ]
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'custLogin/templates/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'custLogin/staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
