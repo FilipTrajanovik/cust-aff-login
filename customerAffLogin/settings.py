5f70462285956a96448a73535e2481a3c8fbb396
Filip <filiptrajanovik@gmail.com>
2025-07-04 14:16:35 +0200

    Changed DATABASE

--- customerAffLogin/settings.py
+++ customerAffLogin/settings.py
@@ -75,12 +75,12 @@ WSGI_APPLICATION = 'customerAffLogin.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-      'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-          'NAME': BASE_DIR / 'db.sqlite3',
-     }
+    #  'default': {
+     #    'ENGINE': 'django.db.backends.sqlite3',
+    #      'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 
-    #'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 
 }
 
